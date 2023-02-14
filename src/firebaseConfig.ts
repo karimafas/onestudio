84ccef96bd3e7a1b8a59de0b8e826cfbaf69fdf8
Karim Afas <karimafas@gmail.com>
2023-02-14 18:32:43 +0000

    task: firebase variables from env and unfocus comment box when posted

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
 const app = initializeApp({
-  apiKey: "AIzaSyDkmZFdUtsAsGs-taErOSzSekuNJX55Kvg",
-  authDomain: "onestudio-91c33.firebaseapp.com",
-  projectId: "onestudio-91c33",
-  storageBucket: "onestudio-91c33.appspot.com",
-  messagingSenderId: "790599047718",
-  appId: "1:790599047718:web:29eb5cbe40b138187ce4e7",
-  measurementId: "G-12TJWFY2C0",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 });
 
 // Firebase storage reference
